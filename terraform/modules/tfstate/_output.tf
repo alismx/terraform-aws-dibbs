bffafa8c24eac6c5b250018f731d035ee3e613af
alismx <alismxwll@gmail.com>
2024-10-10 14:21:46 -0400

    setup to be a pulblishable terraform module

--- terraform/modules/tfstate/_output.tf
+++
@@ -1,7 +0,0 @@
-output "state_bucket" {
-  value = aws_s3_bucket.tfstate
-}
-
-output "dynamodb_table" {
-  value = aws_dynamodb_table.tfstate_lock
-}
--- terraform/utilities/generate_tf_docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-terraform-docs markdown table --output-file README.md --output-mode inject ../modules/ecs
-terraform-docs markdown table --output-file README.md --output-mode inject ../implementation/ecs
-terraform-docs markdown table --output-file README.md --output-mode inject ../implementation/setup
